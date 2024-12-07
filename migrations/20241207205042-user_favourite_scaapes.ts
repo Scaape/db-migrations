604a01f98a6fcf17d620339f5639c85ed563361f
fazenecture <vivekkumarsingh2002@gmail.com>
2024-12-08 02:24:10 +0530

    feat: added table user_favourite_scaapes

---
+++ migrations/20241207205042-user_favourite_scaapes.ts
@@ -0,0 +1,88 @@
+import { QueryInterface, DataTypes } from "sequelize";
+
+export default {
+  up: async (queryInterface: QueryInterface) => {
+    await queryInterface.createTable(
+      "user_favourite_scaapes",
+      {
+        id: {
+          allowNull: false,
+          type: DataTypes.UUID,
+          primaryKey: true,
+          defaultValue: DataTypes.UUIDV4,
+        },
+        user_id: {
+          type: DataTypes.UUID,
+          allowNull: false,
+          references: {
+            model: "users", // Table name
+            key: "id", // Column in the referenced table
+          },
+          onUpdate: "CASCADE",
+          onDelete: "CASCADE",
+        },
+        scaape_id: {
+          type: DataTypes.UUID,
+          allowNull: false,
+          references: {
+            model: "scaapes", // Table name
+            key: "id", // Column in the referenced table
+          },
+          onUpdate: "CASCADE",
+          onDelete: "CASCADE",
+        },
+        created_at: {
+          type: DataTypes.DATE,
+          allowNull: false,
+          defaultValue: DataTypes.NOW,
+        },
+        created_by: {
+          type: DataTypes.UUID,
+          allowNull: true,
+          references: {
+            model: "users", // Table name
+            key: "id", // Column in the referenced table
+          },
+          onUpdate: "CASCADE",
+          onDelete: "SET NULL",
+        },
+        updated_at: {
+          type: DataTypes.DATE,
+          allowNull: true,
+        },
+        updated_by: {
+          type: DataTypes.UUID,
+          allowNull: true,
+          references: {
+            model: "users", // Table name
+            key: "id", // Column in the referenced table
+          },
+          onUpdate: "CASCADE",
+          onDelete: "SET NULL",
+        },
+        deleted_at: {
+          type: DataTypes.DATE,
+          allowNull: true,
+        },
+        deleted_by: {
+          type: DataTypes.UUID,
+          allowNull: true,
+          references: {
+            model: "users", // Table name
+            key: "id", // Column in the referenced table
+          },
+          onUpdate: "CASCADE",
+          onDelete: "SET NULL",
+        },
+      },
+      {
+        engine: "InnoDB",
+        charset: "utf8",
+      }
+    );
+  },
+
+  down: async (queryInterface: QueryInterface) => {
+    await queryInterface.dropTable("user_favourite_scaapes");
+  },
+};
